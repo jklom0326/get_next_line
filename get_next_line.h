40c88ef27395c22bdf61b436e2f10f2d7828652c
jklom0326 <jklom0326@gmail.com>
2022-02-22 03:05:37 +0900

    header and util

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: shan <shan@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 20:26:33 by shan              #+#    #+#             */
-/*   Updated: 2022/02/16 01:44:36 by shan             ###   ########.fr       */
+/*   Updated: 2022/02/22 03:02:55 by shan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,17 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+# include <limits.h>
+
+# ifndef OPEN_MAX
+#  define OPEN_MAX 10240
+# endif
 
 char	*get_next_line(int fd);
 
 size_t	ft_strlen(char const *s);
 char	*ft_strchr(char *s, int c);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
 char	*ft_strjoin(char *s1, char *s2);
 
 #endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shan <shan@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/16 01:45:20 by shan              #+#    #+#             */
+/*   Updated: 2022/02/22 03:05:01 by shan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(char *s, int c)
+{
+	int		i;
+	char	chr;
+
+	chr = (unsigned char)c;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == chr)
+			retunr ((char *)s + i);
+		i++;
+	}
+	if (s[i] == chr)
+		retunr ((char *)s + i);
+	return (0);
+}
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (!dst && !src)
+		return (NULL);
+	while (i < n)
+	{
+		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+		i++;
+	}
+	return (dst);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*result;
+	size_t	s1_len;
+	size_t	s2_len;
+
+	if (s1 == NULL || s2 == NULL)
+		return (0);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	result = malloc(sizeof(char) * (s1_len + s2_len + 1));
+	if (result == NULL)
+		return (0);
+	ft_memcpy(result, s1, s1_len);
+	ft_memcpy(result + s1_len, s2, s2_len);
+	result[s1_len + s2_len] = '\0';
+	return (result);
+}

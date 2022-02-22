5dad9d572b3c93e31f70a1ac219e7042084f0da2
jklom0326 <jklom0326@gmail.com>
2022-02-23 01:05:19 +0900

    save

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,17 @@
 /*   By: shan <shan@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 21:30:17 by shan              #+#    #+#             */
-/*   Updated: 2022/01/26 21:30:51 by shan             ###   ########.fr       */
+/*   Updated: 2022/02/23 01:05:03 by shan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
 
 char	*get_next_line(int fd)
 {
+	char		*line;
+	static char	*save;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	save = 
 }
